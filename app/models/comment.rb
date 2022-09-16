dc273b7c2abb03e646b34f5e694781b2c5f9186f
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-09-16 14:05:22 +0000

    Process Data for the application

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,18 @@
 class Comment < ApplicationRecord
+
+  belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
+
+  def self.update_comments_counter(post_id)
+    post = Post.find(post_id)
+    
+    if post.comments_count.nil?
+        post.comments_count = 1
+    else
+        post.comments_count += 1
+    end
+
+    post.save
+  end
+
 end
