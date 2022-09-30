b5a67b0b09727d8d8d7f6ee2c5e33f56eb52d255
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-09-30 13:59:31 +0000

    Setup devise user authentication with db migration and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_113134) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_112902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_113134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -51,9 +51,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_113134) do
     t.integer "posts_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts", column: "post_id"
+  add_foreign_key "comments", "posts", column: "posts_id"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
